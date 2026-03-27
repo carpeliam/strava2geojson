2f688a9523bbdb0e539c11f3a6de59892cc9aaf7
Liam Morley <liam@carpeliam.com>
2026-03-26 21:15:56 -0400

    whoops, html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
-      <footer>Powered by Strava</footer>
+      <body>
+        {children}
+        <footer>Powered by Strava</footer>
+      </body>
     </html>
   );
 }
