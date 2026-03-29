0ceab245861e03bdd39f1b39f930165ef6b0c4d4
Liam Morley <liam@carpeliam.com>
2026-03-28 21:14:27 -0400

    Attempt to cache peaks file when possible

--- scripts/fetch-osm-data.ts
+++ scripts/fetch-osm-data.ts
@@ -1,4 +1,4 @@
-import { existsSync, mkdirSync, writeFileSync } from 'node:fs';
+import { copyFileSync, existsSync, mkdirSync, statSync, writeFileSync } from 'node:fs';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { overpassJson } from 'overpass-ts';
@@ -7,27 +7,44 @@ import { simplifyLine, truncatePoints } from '../src/lib/transforms.ts';
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const cacheDir = join(__dirname, '..', '.next', 'cache');
 const dataDir = join(__dirname, '..', 'src', 'data');
+const peaksCache = join(cacheDir, 'peaks.json');
 const peaksFile = join(dataDir, 'peaks.json');
+const MAX_CACHE_AGE_MS = 7 * 24 * 60 * 60 * 1000; // 1 week
 
-const OVERPASS_QUERY = `
-[out:json][timeout:60];
-
-area["ISO3166-1"="US"][admin_level=2]->.usa;
-
-node
-  ["natural"="peak"]
-  ["name"~"."]
-  ["ele"~"^(9[0-9]{2}|[1-9][0-9]{3,})"]
-  (41,-73.8,47.6,-66.7)
-  (area.usa);
-
-out;
-`;
+function isCached() {
+  if (existsSync(peaksCache)) {
+    const { mtimeMs } = statSync(peaksCache);
+    console.log('Cached file found, created at:', new Date(mtimeMs).toLocaleString());
+    return (Date.now() - mtimeMs < MAX_CACHE_AGE_MS);
+  }
+  return false;
+}
 
 if (existsSync(peaksFile)) {
-  console.log('OSM data already exists; skipping fetch.');
+  console.log('Peaks file already exists at destination output, no further action necessary.');
+  process.exit(0);
+}
+
+if (isCached()) {
+  console.log('Peaks cache still fresh, skipping fetch');
 } else {
+  const OVERPASS_QUERY = `
+  [out:json][timeout:60];
+
+  area["ISO3166-1"="US"][admin_level=2]->.usa;
+
+  node
+    ["natural"="peak"]
+    ["name"~"."]
+    ["ele"~"^(9[0-9]{2}|[1-9][0-9]{3,})"]
+    (41,-73.8,47.6,-66.7)
+    (area.usa);
+
+  out;
+  `;
+
   mkdirSync(dataDir, { recursive: true });
 
   console.log('Fetching OSM data from Overpass…');
@@ -46,8 +63,9 @@ if (existsSync(peaksFile)) {
     ),
   );
 
-  console.log('Writing file…');
-  writeFileSync(peaksFile, JSON.stringify(geojson));
-
-  console.log('Done.');
+  console.log('Writing cache…');
+  writeFileSync(peaksCache, JSON.stringify(geojson));
 }
+console.log('copying cached file to data dir…');
+copyFileSync(peaksCache, peaksFile);
+console.log('Done.');
